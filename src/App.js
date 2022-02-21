26029e48e50356b62b86027944c50157014ca1a6
Himanshu Khaitan <himakhaitan@Himanshus-MacBook-Air.local>
2022-02-22 01:02:30 +0530

    bg setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className="App">
+      {/* <section>
+        <div>
+          <h1 className="header__main whiteTxt">
+            Hack<span className="blueTxt">Box</span>
+          </h1>
+        </div>
+        <img src="/brand/logo.png" />
+      </section> */}
+    </div>
+  );
 }
 
 export default App;
